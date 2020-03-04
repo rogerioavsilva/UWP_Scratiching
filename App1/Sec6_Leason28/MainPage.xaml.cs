ba79254c6df54b6dfd25cf498a6c45c054fea6f4
guitrav <rogerioavsilva@gmail.com>
2020-03-04 18:20:33 -0300

    class 28

---
+++ App1/Sec6_Leason28/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Sec6_Leason28
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public TruckData Data { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var infos = new TruckInfo[]
+            {
+                new TruckInfo() { ID = "1", Name = "Taco Talent", Style = "Mexican" },
+                new TruckInfo() { ID = "2", Name = "Cake Shack", Style = "Desserts" },
+                new TruckInfo() { ID = "3", Name = "Ice Heaven", Style = "Cold Drinks" },
+            };
+
+            var data = new TruckData()
+            {
+                Trucks = new ObservableCollection<TruckInfo>(infos)
+            };
+
+            //    DataContext = data;
+
+            Data = data;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ App1/Sec6_Leason28/StyleToBrushConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Sec6_Leason28
+{
+    public class StyleToBrushConverter : IValueConverter
+    {
+        public SolidColorBrush _default = new SolidColorBrush(Windows.UI.Colors.White);
+        public SolidColorBrush _Mexican = new SolidColorBrush(Windows.UI.Colors.LightPink);
+        public SolidColorBrush _Desserts = new SolidColorBrush(Windows.UI.Colors.Chocolate);
+
+
+        public StyleToBrushConverter()
+        {
+
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            switch (value as string)
+            {
+                case null:
+                default:
+                    return _default;
+                case "Mexican":
+                    return _Mexican;
+                case "Desserts":
+                    return _Desserts;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
