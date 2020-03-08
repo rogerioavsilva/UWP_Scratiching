a2dfe16b8f7f0a1090aa418c0a4adc58cf33bf15
Rogério Silva <rogerioavsilva@gmail.com>
2020-03-08 14:15:27 -0300

    Exercicio 29

---
+++ App1/ViewModels/TruckVM.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class TruckVM : NotificationBase
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Style { get; set; }
+    }
+}
