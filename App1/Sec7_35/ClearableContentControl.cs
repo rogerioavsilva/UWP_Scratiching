33f39fbb9bd4399c7c83c54ca321a201d2d2c560
Rogério Silva <rogerioavsilva@gmail.com>
2020-03-14 22:01:18 -0300

    Exercise 35

---
+++ App1/Sec7_35/ClearableContentControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace Sec7_35
+{
+    public sealed class ClearableContentControl : ContentControl
+    {
+        public ClearableContentControl()
+        {
+            this.DefaultStyleKey = typeof(ClearableContentControl);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _ClearButton = this.GetTemplateChild("PART_ClearButton") as Button;
+            _ClearButton.Click += (s, e) => { this.Content = null; };
+        }
+
+        public Button _ClearButton;
+    }
+}
