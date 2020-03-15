3cf7f9a551a75a982b19ebd11fbdb8c87a384f58
Rogério Silva <rogerioavsilva@gmail.com>
2020-03-15 17:04:39 -0300

    class finished

---
+++ App1/Sec7_36/TextBoxUtils.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Sec7_36
+{
+    public class TextBoxUtils
+    {
+
+
+        public static string GetValidationRegEx(DependencyObject obj)
+        {
+            return (string)obj.GetValue(ValidationRegExProperty);
+        }
+
+        public static void SetValidationRegEx(DependencyObject obj, string value)
+        {
+            obj.SetValue(ValidationRegExProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for ValidationRegEx.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValidationRegExProperty =
+            DependencyProperty.RegisterAttached("ValidationRegEx", typeof(string), typeof(TextBoxUtils), 
+                new PropertyMetadata(null, (obj, args)=> 
+                {
+                    var tb = obj as TextBox;
+                    if (tb == null)
+                        return;
+                    if(args.NewValue != null)
+                    {
+                        if (args.OldValue == null)
+                            tb.TextChanged += Tb_TextChanged;
+                    }
+                    else
+                    {
+                        var ecn = TextBoxUtils.GetErrorControlName(tb);
+                        if(ecn != null)
+                        {
+                            var uie = (UIElement)tb.FindName(ecn);
+                            if (uie != null)
+                                uie.Visibility = Visibility.Collapsed;
+                        }
+                        tb.TextChanged -= Tb_TextChanged;
+                    }
+                }));
+
+        public static void Tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateTextBox(sender as TextBox);
+        }
+
+        public static void ValidateTextBox(TextBox textBox)
+        {
+            var exp = TextBoxUtils.GetValidationRegEx(textBox);
+            var errorControlName = TextBoxUtils.GetErrorControlName(textBox);
+
+            if (errorControlName == null)
+                return;
+
+            var uie = (UIElement)textBox.FindName(errorControlName);
+
+            if (uie == null)
+                return;
+
+            bool isValid = true;
+
+            if(string.IsNullOrEmpty(exp) == false && textBox.Text != null)
+            {
+                var re = new Regex(exp);
+                isValid = re.IsMatch(textBox.Text);
+            }
+            uie.Visibility = isValid ? Visibility.Collapsed : Visibility.Visible;
+
+        }
+
+        public static string GetErrorControlName(DependencyObject obj)
+        {
+            return (string)obj.GetValue(ErrorControlNameProperty);
+        }
+
+        public static void SetErrorControlName(DependencyObject obj, string value)
+        {
+            obj.SetValue(ErrorControlNameProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for ErrorControlName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ErrorControlNameProperty =
+            DependencyProperty.RegisterAttached("ErrorControlName", typeof(string), typeof(TextBoxUtils),
+                 new PropertyMetadata(null, (obj, args) =>
+                 {
+                     var tb = obj as TextBox;
+                     if (tb == null)
+                         return;
+                     if (args.OldValue != null)
+                     {
+                         var uie = (UIElement)tb.FindName(args.OldValue as string);
+
+                         if (uie != null)
+                             uie.Visibility = Visibility.Collapsed;
+                     }
+                     ValidateTextBox(tb);
+
+                 }));
+
+
+    }
+}
--- App1/Sec7_36/UniformWidthPanel.cs
+++ App1/Sec7_36/UniformWidthPanel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Sec7_36
@@ -13,6 +14,42 @@ namespace Sec7_36
         public UniformWidthPanel()
         {
 
+        }
+
+
+
+        public static double GetScale(DependencyObject obj)
+        {
+            return (double)obj.GetValue(ScaleProperty);
+        }
+
+        public static void SetScale(DependencyObject obj, double value)
+        {
+            obj.SetValue(ScaleProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Scale.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ScaleProperty =
+            DependencyProperty.RegisterAttached("Scale", typeof(double), typeof(UniformWidthPanel),
+                new PropertyMetadata(1.0, (o, a) =>
+                {
+                    var uwp = GetAncestor<UniformWidthPanel>(o as FrameworkElement);
+                    if (uwp != null)
+                    {
+                        uwp.InvalidateMeasure();
+                        uwp.UpdateLayout();
+                    }
+                }));
+
+        private static T GetAncestor<T>(FrameworkElement fe)
+        {
+            var parent = Windows.UI.Xaml.Media.VisualTreeHelper.GetParent(fe);
+            if (parent == null)
+                return default(T);
+            if (parent is T)
+                return (T)(object)parent;
+            else
+                return GetAncestor<T>(parent as FrameworkElement);
         }
 
         protected override Size MeasureOverride(Size availableSize)
@@ -26,14 +63,29 @@ namespace Sec7_36
 
             double NumChildren = this.Children.Count;
 
-            if(NumChildren > 0)
+            if (NumChildren > 0)
             {
-                Size childSize = availableSize;
-                childSize.Width /= NumChildren;
+                double totalScale = 0.0;
                 for (int i = 0; i < NumChildren; i++)
                 {
                     var child = this.Children[i];
-                    child.Measure(childSize);
+                    totalScale += UniformWidthPanel.GetScale(child);
+                }
+
+                //Size childSize = availableSize;
+                //childSize.Width /= NumChildren;
+
+
+                for (int i = 0; i < NumChildren; i++)
+                {
+                    var child = this.Children[i];
+
+                    Size scaledChildSize = availableSize;
+                    var childScale = UniformWidthPanel.GetScale(child);
+                    scaledChildSize.Width *= childScale / totalScale;
+
+
+                    child.Measure(scaledChildSize);
 
                     if (child.DesiredSize.Height > newSize.Height)
                         newSize.Height = child.DesiredSize.Height;
@@ -49,12 +101,22 @@ namespace Sec7_36
             double numChildren = this.Children.Count;
             if (numChildren > 0)
             {
+                double totalScale = 0.0;
+                for (int i = 0; i < numChildren; i++)
+                {
+                    var child = this.Children[i];
+                    totalScale += UniformWidthPanel.GetScale(child);
+                }
+
+                double accumulatedScale = 0.0;
                 var newLeft = 0;
                 for (int i = 0; i < numChildren; i++)
                 {
-                    var newRight = (int)Math.Round(finalSize.Width / numChildren * (i + 1));
-                    var newWidth = newRight - newLeft;
                     var child = this.Children[i];
+                    accumulatedScale += UniformWidthPanel.GetScale(child);
+                    //var newRight = (int)Math.Round(finalSize.Width / numChildren * (i + 1));
+                    var newRight = (int)Math.Round(finalSize.Width * accumulatedScale / totalScale);
+                    var newWidth = newRight - newLeft;
                     Rect childRect = new Rect(newLeft, 0, newWidth, finalSize.Height);
                     child.Arrange(childRect);
                     newLeft += newWidth;
