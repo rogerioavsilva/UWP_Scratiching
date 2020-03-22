d3bd39ffed267c0d67d48bafff919f646efdd1df
Rogério Silva <rogerioavsilva@gmail.com>
2020-03-22 15:38:01 -0300

    finished classes 40 41 42

---
+++ App1/Sec8_41/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Sec8_41
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(320, 320));
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBoundsChanged += MainPage_VisibleBoundsChanged;
+
+
+        }
+
+        private void MainPage_VisibleBoundsChanged(Windows.UI.ViewManagement.ApplicationView sender, object obj)
+        {
+            var Width = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBounds.Width;
+            if (Width >= 720)
+            {
+                SView.DisplayMode = SplitViewDisplayMode.CompactInline;
+                SView.IsPaneOpen = true;
+            }
+            else if (Width >= 720)
+            {
+                SView.DisplayMode = SplitViewDisplayMode.CompactOverlay;
+                SView.IsPaneOpen = false;
+            }
+            else
+            {
+                SView.DisplayMode = SplitViewDisplayMode.Overlay;
+                SView.IsPaneOpen = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SView.IsPaneOpen = !SView.IsPaneOpen;
+        }
+    }
+}
---
+++ App1/Sec8_41/MyUserControl1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Sec8_41
+{
+    public sealed partial class MyUserControl1 : UserControl
+    {
+        public MyUserControl1()
+        {
+            this.InitializeComponent();
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBoundsChanged += MyUserControl1_VisibleBoundsChanged;
+        }
+
+        private void MyUserControl1_VisibleBoundsChanged(Windows.UI.ViewManagement.ApplicationView sender, object args)
+        {
+            var width = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBounds.Width;
+            if(width >= 360)
+            {
+                RelativePanel.SetBelow(FavText, null);
+                RelativePanel.SetRightOf(FavText, Stars);
+
+                RelativePanel.SetAlignVerticalCenterWith(FavText, Stars);
+                RelativePanel.SetAlignVerticalCenterWithPanel(Stars, true);
+            }
+            else
+            {
+                RelativePanel.SetBelow(FavText, null);
+                RelativePanel.SetRightOf(FavText, Stars);
+
+                RelativePanel.SetAlignVerticalCenterWith(FavText, null);
+                RelativePanel.SetAlignVerticalCenterWithPanel(Stars, false);
+            }
+        }
+    }
+}
